47c37cfc808df0016d7ff4e938c050d5856a0fc1
bobokrut <egor-evlampiev@mail.ru>
2023-05-12 00:21:40 +0200

    🔒 chore: update my_dash_component module to comply with new business logic

--- my_dash_component/Grid.py
+++ my_dash_component/Grid.py
@@ -7,35 +7,39 @@ class Grid(Component):
     """A Grid component.
 
 
-Keyword arguments:
+    Keyword arguments:
 
-- children (list of a list of or a singular dash component, string or numbers; required)
+    - id (string; optional):
+        Unique ID to identify this component in Dash callbacks.
 
-- id (string; optional):
-    Unique ID to identify this component in Dash callbacks.
+    - graths (list of a list of or a singular dash component, string or numbers; required)
 
-- hash (string; required)"""
-    _children_props = []
-    _base_nodes = ['children']
-    _namespace = 'my_dash_component'
-    _type = 'Grid'
+    - hash (string; required)"""
+
+    _children_props = ["graths"]
+    _base_nodes = ["graths", "children"]
+    _namespace = "my_dash_component"
+    _type = "Grid"
+
     @_explicitize_args
-    def __init__(self, children=None, hash=Component.REQUIRED, id=Component.UNDEFINED, **kwargs):
-        self._prop_names = ['children', 'id', 'hash']
-        self._valid_wildcard_attributes =            []
-        self.available_properties = ['children', 'id', 'hash']
-        self.available_wildcard_properties =            []
-        _explicit_args = kwargs.pop('_explicit_args')
+    def __init__(
+        self,
+        graths=Component.REQUIRED,
+        hash=Component.REQUIRED,
+        id=Component.UNDEFINED,
+        **kwargs,
+    ):
+        self._prop_names = ["id", "graths", "hash"]
+        self._valid_wildcard_attributes = []
+        self.available_properties = ["id", "graths", "hash"]
+        self.available_wildcard_properties = []
+        _explicit_args = kwargs.pop("_explicit_args")
         _locals = locals()
         _locals.update(kwargs)  # For wildcard attrs and excess named props
-        args = {k: _locals[k] for k in _explicit_args if k != 'children'}
+        args = {k: _locals[k] for k in _explicit_args}
 
-        for k in ['hash']:
+        for k in ["graths", "hash"]:
             if k not in args:
-                raise TypeError(
-                    'Required argument `' + k + '` was not specified.')
+                raise TypeError("Required argument `" + k + "` was not specified.")
 
-        if 'children' not in _explicit_args:
-            raise TypeError('Required argument children was not specified.')
-
-        super(Grid, self).__init__(children=children, **args)
+        super(Grid, self).__init__(**args)
--- my_dash_component/_imports_.py
+++ my_dash_component/_imports_.py
@@ -2,8 +2,4 @@ from .Container import Container
 from .Grid import Grid
 from .Navbar import Navbar
 
-__all__ = [
-    "Container",
-    "Grid",
-    "Navbar"
-]
+__all__ = ["Container", "Grid", "Navbar"]
