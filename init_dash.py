8a612872dcbde72fe99be4eea0f0922bf8683cec
bobokrut <egor-evlampiev@mail.ru>
2023-05-12 00:13:53 +0200

    🐛 fix(init_dash.py): fix blueprint registration for user authentication and add login manager initialization ✨ feat(init_dash.py): add secret key configuration and login view configuration for login manager 🚀 chore(pyproject.toml): update dependencies versions 🚀 chore(requirements.txt): update dependencies versions 🎨 style(templates/login.html): comment out unused code in login page template

--- init_dash.py
+++ init_dash.py
@@ -1,10 +1,15 @@
 import dash
+from src.user_auth import auth, login_manager
 from flask import Flask
-from src.user_auth import user
 from flask_login import login_required
 
 server = Flask(__name__)
-server.register_blueprint(user)
+server.config["SECRET_KEY"] = "1234"
+server.register_blueprint(auth)
+login_manager.init_app(server)
+login_manager.login_view = "auth.login_get"
+
+
 app = dash.Dash(
     __name__,
     server=server,
@@ -15,7 +20,8 @@ app = dash.Dash(
         "/static/react-grid-layout-css.css",
     ],
 )
-
-# for view_func in server.view_functions:
-#     if view_func.startswith("/dash/"):
-#         server.view_functions[view_func] = login_required(server.view_functions[view_func])
+for view_func in server.view_functions:
+    if view_func.startswith("/dash/"):
+        server.view_functions[view_func] = login_required(
+            server.view_functions[view_func]
+        )
