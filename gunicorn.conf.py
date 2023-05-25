addcaa5f1c948021b0b8c5fecd78903103cf08c8
bobokrut <egor-evlampiev@mail.ru>
2023-05-25 09:46:17 +0200

    🔥 chore(gunicorn.conf.py): remove gunicorn configuration file as it is no longer needed

--- gunicorn.conf.py
+++
@@ -1,8 +0,0 @@
-import multiprocessing
-
-workers = multiprocessing.cpu_count() * 2 + 1
-bind = "0.0.0.0:8000"
-wsgi_app = "src.main:create_server()"
-pidfile = "master.pid"
-worker_class = "gevent"
-
