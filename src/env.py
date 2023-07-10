57decce80de848c8a767fd98f5794c57c77e95c9
bobokrut <egor-evlampiev@mail.ru>
2023-07-10 12:08:19 +0200

    🔒 chore(env.py): add validation for SECRET_KEY and GEOCODING_KEY to ensure they are set

--- src/env.py
+++ src/env.py
@@ -5,5 +5,9 @@ load_dotenv()
 
 SECRET_KEY = environ.get("SECRET_KEY")
 GEOCODING_KEY = environ.get("GEOCODING_KEY")
+
 if not GEOCODING_KEY:
     raise ValueError("GEOCODING_KEY is not set")
+
+if not SECRET_KEY:
+    raise ValueError("SECRET_KEY is not set")
