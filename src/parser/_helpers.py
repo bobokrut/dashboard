918d4c2e0671911fe93d8dbe3fd718ef7e9a099e
bobokrut <egor-evlampiev@mail.ru>
2023-07-10 11:40:26 +0200

    ✨ feat(parser): add initial implementation of parser module

---
+++ src/parser/_helpers.py
@@ -0,0 +1,92 @@
+import json
+import jsonschema
+from jsonschema.validators import RefResolver
+import requests
+
+current_url = ""
+
+
+def expand_json_schema(schema):
+    # Create a resolver to resolve references
+    resolver = RefResolver.from_schema(schema)
+
+    def expand_refs(refs):
+        global current_url
+        # Recursive function to expand references
+        if isinstance(refs, dict):
+            if "$ref" in refs:
+                ref = refs["$ref"]
+                if ref.startswith("http://") or ref.startswith("https://"):
+                    # Fetch the remote reference and expand it recursively
+                    current_url = ref
+                    resolved = resolver.resolve(ref)
+                    return expand_refs(resolved)
+                elif ref.startswith("#"):
+                    # Resolve the reference and expand it recursively
+                    ref = (
+                        current_url.split("#")[0] + ref
+                        if "#" in current_url
+                        else current_url + ref
+                    )
+                    resolved = resolver.resolve(ref)
+                    return expand_refs(resolved[1])
+                else:
+                    # Resolve the reference and expand it recursively
+                    resolved = resolver.resolve(ref)
+                    return expand_refs(resolved[1])
+            else:
+                # Expand references in each value of the dictionary
+                return {key: expand_refs(value) for key, value in refs.items()}
+        elif isinstance(refs, list):
+            # Expand references in each item of the list
+            return [expand_refs(item) for item in refs]
+        else:
+            return refs
+
+    # Expand references in the top-level schema
+    expanded_schema = expand_refs(schema)
+
+    return expanded_schema
+
+
+def fetch_remote_schema(uri):
+    # Fetch the remote JSON schema
+    response = requests.get(uri)
+    remote_schema = response.json()
+    return remote_schema
+
+
+def get_all_keys(json_schema):
+    keys = []
+    required_keys = []
+
+    def traverse(schema):
+        nonlocal keys, required_keys
+
+        if isinstance(schema, dict):
+            if "properties" in schema:
+                for key, value in schema["properties"].items():
+                    keys.append(key)
+                    if "required" in schema and key in schema["required"]:
+                        required_keys.append(key)
+                    traverse(value)
+            if "items" in schema:
+                traverse(schema["items"])
+            if "oneOf" in schema:
+                for item in schema["oneOf"]:
+                    traverse(item)
+            if "allOf" in schema:
+                for item in schema["allOf"]:
+                    traverse(item)
+
+    traverse(json_schema)
+
+    return keys, required_keys
+
+
+# Expand the schema
+# expanded_schema = expand_json_schema(schema)
+# keys, required = extract_data_keys(expanded_schema)
+#
+# with open("schema.json", "w") as f:
+#     json.dump(expanded_schema, f, indent=4)
---
+++ src/parser/_parser.py
@@ -0,0 +1,147 @@
+from dateutil.parser import parse as parse_date
+
+from typing import Any, NewType
+from collections import namedtuple
+from types import FunctionType
+from typing import TypeAlias
+import re
+
+Url = NewType("Url", str)
+Position = namedtuple("Position", ["lat", "lon"])
+
+FiwareJson: TypeAlias = list[dict[str, Any]]
+
+
+class ParserException(Exception):
+    """Exception raised when a parser is not found"""
+
+
+class NotNeeded(Exception):
+    """Exception raised when a value is not needed."""
+
+    pass
+
+
+#########################################################
+#                      Location                         #
+#########################################################
+
+
+def _parse_location(
+    location: dict[str, Any]
+) -> Position | list[Position] | list[list[Position]]:
+    match location["value"]["type"]:
+        case "Point":
+            return Position(
+                lon=location["value"]["coordinates"][0],
+                lat=location["value"]["coordinates"][1],
+            )
+        case "LineString":
+            return [
+                Position(lon=lon, lat=lat)
+                for lon, lat in location["value"]["coordinates"]
+            ]
+        case "Polygon":
+            return [
+                Position(lon=lon, lat=lat)
+                for lon, lat in location["value"]["coordinates"][0]
+            ]
+        case "MultiPoint":
+            return [
+                Position(lon=lon, lat=lat)
+                for lon, lat in location["value"]["coordinates"]
+            ]
+        case "MultiLineString":
+            return [
+                [Position(lon=p[0], lat=p[1]) for p in line_string]
+                for line_string in location["value"]["coordinates"]
+            ]
+        case _:
+            raise NotImplemented
+
+
+def _parse_address(address: dict[str, Any]) -> str:
+    return_address = ""
+    address = address["value"]
+
+    if "streetAddress" in address:
+        return_address += (
+            address["streetAddress"]
+            if not "streerNr" in address
+            else f"{address['streetAddress']} {address['streetNr']}, "
+        )
+
+    if "postalCode" in address:
+        return_address += address["postalCode"] + ", "
+
+    if "addressLocality" in address:
+        return_address += address["addressLocality"] + ", "
+
+    if "addressRegion" in address:
+        return_address += address["addressRegion"] + ", "
+
+    if "addressCountry" in address:
+        return_address += address["addressCountry"]
+
+    return return_address
+
+
+def get_parsers() -> dict[str, FunctionType]:
+    return {
+        name: obj for name, obj in globals().items() if isinstance(obj, FunctionType)
+    }
+
+
+def __parse_old(data: FiwareJson, keys: list[str]) -> list[list[Any]]:
+    to_return = []
+    parsers = get_parsers()
+    try:
+        for d in data:
+            to_return.append([parsers[f"parse_{key}"](d[key]) for key in keys])
+
+        return to_return
+
+    except KeyError as e:
+        raise ParserException(f"Parser for {e} not found")
+
+
+def _parse(data: FiwareJson, keys: list[str]) -> list[list[Any]]:
+    to_return = []
+    parsers = get_parsers()
+
+    for d in data:
+        l = []
+        for key in keys:
+            item = d[key]
+
+            if (
+                f"_parse_{key}" in parsers
+            ):  # If there is a custom parser to parse an object
+                l.append(parsers[f"_parse_{key}"](item))
+
+            elif isinstance(item, str) and re.fullmatch(
+                r"\b\d{4}-\d{2}-\d{2}T\d{2}:\d{2}:\d{2}Z\b", item
+            ):  # If the item is a date
+                l.append(parse_date(item))
+
+            elif isinstance(item, str):
+                l.append(item)
+
+            elif (
+                isinstance(item, dict)
+                and "value" in item
+                and not isinstance(item["value"], dict)
+            ):
+                l.append(item["value"])
+
+            else:
+                raise ParserException(f"Parser for {key} not found\nValue: {item}")
+
+        to_return.append(l)
+
+    return to_return
+
+
+def parse(data: FiwareJson, keys: list[str]) -> list[list[Any]]:
+    """Parses data from the Fiware with the given data keys"""
+    return _parse(data, keys)
